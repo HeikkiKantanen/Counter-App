7beba0c458cd2e028c817c105ded6640e097f3f4
BC-macair015 <bc-macair015@BC-macair015s-MacBook-Air.local>
2021-10-26 12:33:23 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                <small> Made by Heikki Kantanen &copy; Copyright 2021</small>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    state = {
+        counter: 0,
+    };
+
+    addOne = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+    
+    removeOne = () => {
+        if (this.state.counter !== 0) {
+        this.setState ({
+            counter: this.state.counter - 1
+        });
+    }
+    };
+
+    reset = () => {
+        this.setState ({
+            counter: 0
+        });
+    };
+
+    addFive = () => {
+        this.setState ({
+            counter: this.state.counter +5
+        });
+    };
+
+    removeFive = () => {
+        this.setState ({
+            counter: this.state.counter -5
+        });
+    };
+
+    render() {
+        let circleClass = "";
+
+        if (this.state.counter === 0) {
+            circleClass = "circle";
+        } else if (this.state.counter % 2 === 0) {
+            circleClass = "circle even"
+        } else {
+            circleClass ="circle odd"
+        };
+        
+
+
+
+        return (
+            <div className="main">
+                <div className="circle-wrapper">
+                    <h1 className={circleClass}>{this.state.counter}</h1>
+                </div>
+                <div className="button-wrapper">
+                    <button onClick={this.addOne}>Add one</button>
+                    <button onClick={this.removeOne}>Remove one</button>
+                    <button onClick={this.reset}>Reset</button>
+                    <button onClick={this.addFive}>Add five</button>
+                    <button onClick={this.removeFive}>Remove five</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
